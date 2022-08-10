da38fe02b56da21083787964c6fd64c940835fbc
David <davidspader97@gmail.com>
2022-08-10 19:10:41 -0300

    add products to admin dashboard

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -2,17 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class AdminProductController extends Controller
 {
     public function index()
     {
-        return view('admin.products');
+        $products = Product::all();
+        return view('admin.products', compact('products'));
     }
 
     public function edit()
     {
         return view('admin.product_edit');
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function create()
+    {
+        return view('admin.product_create');
+    }
+
+    public function store()
+    {
+
     }
 }
