c6a19306c16ef397197385e96755392b78d023e1
David <davidspader97@gmail.com>
2022-08-11 22:04:47 -0300

    add search

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,3 +1,4 @@
+@php use Illuminate\Support\Facades\Storage; @endphp
 @extends('layouts.default')
 
 @section('content')
@@ -5,15 +6,15 @@
         <div class="container px-5 py-24 mx-auto">
             <div class="lg:w-4/5 mx-auto flex flex-wrap">
                 <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-                     src="{{ $product->cover }}">
+                     src="{{ Storage::url($product->cover) }}">
                 <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                     <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{ $product->name }}</h1>
                     <p class="leading-relaxed">{{ $product->description }}</p>
                     @if($product->stock)
-                    <div class="my-3">
+                        <div class="my-3">
                         <span
                             class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800">Em estoque</span>
-                    </div>
+                        </div>
                     @endif
                     <div class="flex border-t-2 border-gray-100 mt-6 pt-6">
                         <span class="title-font font-medium text-2xl text-gray-900">{{ $product->price }}</span>
